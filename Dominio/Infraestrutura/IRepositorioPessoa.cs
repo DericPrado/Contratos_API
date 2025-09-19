05b6ca056b69ced1cb7394b630fbd8213fe7e701
DericPrado <dericprado@gmail.com>
2025-09-19 14:47:32 -0300

    Adicionando busca por Id no contexto de pessoa

--- Dominio/Infraestrutura/IRepositorioPessoa.cs
+++ Dominio/Infraestrutura/IRepositorioPessoa.cs
@@ -4,6 +4,7 @@ public interface IRepositorioPessoa
     public Task<bool> AtualizarPessoa(Pessoa pessoa);
     public Task<bool> DeletarPessoa(Guid id);
     public Task<Pessoa?> ObterPessoaPorDocumento(string documento);
+    public Task<Pessoa?> ObterPessoaPorId(Guid id);
     public Task<List<Pessoa>> ObterTodasPessoasAtivas();
 
 }
